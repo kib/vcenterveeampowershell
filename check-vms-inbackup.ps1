8e6288e620cacad28e8bc8b8a99a2cc462259e76
Ejal de Klerk <edeklerk@gmail.com>
2017-02-22 17:06:45 +0100

    Veeam backup scripts (initial)
    Check whether VMs are included in the backup, and make an overview f th backup schedule in VEEAM

---
+++ check-vms-inbackup.ps1
@@ -0,0 +1,187 @@
+﻿<#
+Script to run against veeam to check if VM's are included in backup jobs
+
+2015-12-17 Rikkert Kooy: Changed method to get VMs in any job. So VM's were returned even if job hadn't run yet
+#>
+
+[CmdletBinding()]
+Param()
+
+
+#region Module 1 : Customization
+    Write-Verbose "starting script and loading custom settings from .\custom_Settings.xml"
+    [xml]$ConfigFile = Get-Content ".\custom_Settings.xml"
+
+    #vcenter settings from settings.xml
+    $vcenter = $ConfigFile.settings.OtherSettings.Vcenter
+
+    # Email settings from Settings.xml
+    $MailTo = $ConfigFile.Settings.EmailSettings.MailTo
+    $MailFrom = $ConfigFile.Settings.EmailSettings.MailFrom
+    $MailServer = $ConfigFile.Settings.EmailSettings.SMTPServer
+    $excludedvms= $ConfigFile.Settings.Exclusions.VM
+
+    if($MailTo){$SendMail=$True}
+#endregion Module 1: Customization
+
+#region Module 2 : Function
+    
+    <#
+    .Synopsis
+       Creates the email body
+    .DESCRIPTION
+       Long description
+    #>
+    function Create-MailBody
+    {
+        [CmdletBinding()]
+        [OutputType([string])]
+        Param
+        (
+            # Param1 help description
+            [Parameter(Mandatory=$true,
+                       ValueFromPipelineByPropertyName=$true,
+                       Position=0)]
+            $excludedVMs,
+    
+            # Param2 help description
+            [Parameter(Mandatory=$true,
+                       ValueFromPipelineByPropertyName=$true,
+                       Position=1)]
+            $notBackedupVMs,
+            
+            [Parameter(Mandatory=$true,
+                       ValueFromPipelineByPropertyName=$true,
+                       Position=2)]
+            $backupedVMs
+        )
+        Write-Verbose "Staring the creation of the html mail body"
+        $body = '<body style="font-family: Arial; font-size: 10pt;">'
+        $body += '<b>Here is the bi-weekly report of the backup</b>'
+        $body +='<br><br>'
+
+        $body +='<b>Below VMs are not included in a backup job</b>'
+        
+        foreach($vm in $excludedVMs)
+        {
+            $body +="<br>"+$vm.Name +":   "+$vm.Value
+        }
+
+
+        $body +="<br><br>"
+        $body +="<b>Below VMs have not been backuped in the past 7 days</b>"
+        foreach($vm in $notBackedupVMs)
+        {
+            $body +="<br>" + $vm.Name +":   "+$vm.Value
+        }
+
+        $body +="<br><br>"
+        $body +="<b>Below VMs have been backuped in past 7 days</b>"
+        foreach($vm in $backedupVMs)
+        {
+            $body +="<br>" +$vm.Name +":   "+$vm.Value
+        }
+        
+        Write-Verbose "Email body is:"
+        Write-Verbose "$body"    
+        return $body
+    }
+
+#endregion Module 2: Function
+
+
+#region Module 3: load powershell modules
+    Write-Verbose "loading VMware and Veeam powershell snapin"
+
+    Add-PSSnapin "VMware.VimAutomation.Core" -ErrorAction SilentlyContinue
+
+    Add-PSSnapin VeeamPSSnapIn -ErrorAction SilentlyContinue
+#endregion Module 3: load powershell modules
+
+Write-Verbose "Connection to vcenter server: $vcenter" 
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false | out-null
+Connect-ViServer $vcenter | Out-Null
+
+#Get a list of all VMs from vCenter
+Write-Verbose "Getting list of all VMs in vCenter"
+$vms = Get-VM | ForEach-Object {$_ | Select-object @{Name="VMname";Expression={$_.Name}}}| sort vmname|  select vmname -Unique
+
+
+# Find all backup jobs that have ended in the last 7 days
+Write-Verbose "Finding backup jobs in Veeam"
+$backupSessionsPast7Days = Get-VBRBackupSession | Where-Object {$_.jobtype -eq 'backup' -and $_.creationtime -ge (Get-Date).adddays(-7)}
+$backupJobs = Get-VBRJob | Where-Object {$_.IsBackup}
+
+
+# Find objects in a backup jobs
+$backupSessions = @()
+Write-Verbose "Finding VMs that are included in a backup job"
+foreach ($backupjob in $backupjobs) {
+    $backupSessions += $backupjob.GetObjectsInJob()
+}
+
+Write-Verbose "Processing collected data to create hashtable with values whether the vm is unprotected, included in a job or succesfully backued up"
+# Reduce to list of VM names in succesful backup sessions
+
+
+Write-Verbose "Finding VMs that have backup status success or warning in the past 7 days"
+
+#in backupjobs of past 7 days find all the backups of specifics VMs that didn't fail and make the list unique.
+$backedupVMs = $backupSessionsPast7Days | ForEach-Object{($_.gettasksessions() | 
+                        where{$_.status -ne 'failed'}).name} | select -Unique @{Name="VMname";Expression={$_}}
+
+
+# Reduce to list of VM names in backup sessions
+$includedVMs = $backupSessions | ForEach-Object {$_ | Select-object @{Name="VMname";Expression={$_.Name}}}
+
+# Build hash table with excluded VMs
+$excludedvmhash= @{}
+foreach ($vm in $excludedvms) {
+    $excludedvmhash.Add($vm, "Excluded")
+}
+
+# Build hash table of VMs from vCenter, skipping excluded VMs
+# assume VM is not protected
+$vmhash= @{}
+foreach ($vm in $vms) {
+    if (!$excludedvmhash.ContainsKey($vm.VMname)) {
+        $vmhash.Add($vm.VMname, "Unprotected")
+    }
+}
+
+# Loop through VMs included in backup job, if VM name exist in hash
+# update value to included in job
+foreach($includedVM in $includedVMs) {
+    if($vmhash.ContainsKey($includedVM.VMname)) {
+        $vmhash[$includedVM.VMname]="Included in job"
+    }
+}
+
+# Loop through backed up VMs, if VM name exist in hash
+# update value to protected
+foreach($backedupVM in $backedupVMs) {
+    if($vmhash.ContainsKey($backedupvm.VMname)) {
+        $vmhash[$backedupvm.VMname]="Protected"
+    }
+}
+Write-Verbose "processing is done"
+
+Write-Verbose "Starting sorting data to generate report to mail"
+
+#VMs not included in any job
+$notIncludedVM = $vmhash.GetEnumerator() | where {$_.value -eq "unprotected"} | sort name
+if($notIncludedVM -eq $null){$notIncludedVM= @()}
+
+$notBackedupVM = $vmhash.GetEnumerator() | where {$_.Value -eq "Included in job"} | sort name
+if($notBackedupVM -eq $null){$notBackedupVM = @()}
+
+$backedupVMs = $vmhash.GetEnumerator() | where {$_.Value -eq "Protected"} | sort name
+
+$subject = "Bi-Weekly report of Veeam backup"
+
+$htmlBody = Create-MailBody -excludedVMs $notIncludedVM -notBackedupVMs $notBackedupVM -backupedVMs $backedupVMs
+
+if($SendMail) {
+    Send-MailMessage -subject $subject -SmtpServer $MailServer -BodyAsHtml $htmlBody -to $MailTo -From $MailFrom
+}
+
---
+++ get-backupschedule.ps1
@@ -0,0 +1,230 @@
+﻿<#script created to make the following visible:
+  - when Veeam backup jobs start
+  - how big the fulls are
+  - when the monthly active full runs (it is currently assumed the active fulls run each month)
+ #>
+
+[CmdletBinding()]
+param()
+
+
+#region 1 module: functions
+
+
+
+<#
+.Synopsis
+   Converts the Veeam ApproxSizeString to an actual integer in Bytes
+.DESCRIPTION
+   Converts a string in format '123.3 GB' to an integer with value 123.
+   Only values in MB, GB and TB are accepted.
+.EXAMPLE
+   Convert-SizeStringtoSizeInteger '123.3 GB'
+.EXAMPLE
+   $string = '1.8 TB'
+   convert-SizeStringtoSizeInteger $string
+#>
+function Convert-SizeStringtoSizeInteger
+{
+    [CmdletBinding()]
+    [OutputType([int64])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [String]
+        $sizeString
+    )
+    #find if the size is in MB, GB or TB
+    $sizeNotation = $sizeString.Substring($sizeString.Length-2)
+    
+    $size = [double]($sizeString.Substring(0,$sizeString.Length-3))
+    write-verbose "Received $sizeString.`nSeperated this into value: $size with notation $sizeNotation"
+    switch ($sizeNotation)
+    {
+        'GB' {$power = 3; break}
+        'TB' {$power = 4; break}
+        'MB' {$power = 2 ;break}
+        Default {Write-Verbose "$sizeNotation is invalid format exiting script.`nReturning 0 as value";$power='ERROR'}
+    }
+    write-verbose "Will multiply $size with 1024^$power"
+    if($power -eq 'ERROR'){$sizeBytes =[int64] 0}
+    else{$sizeBytes = [int64] ($size * [math]::Pow(1024,$power))}
+    Return $sizeBytes
+}
+
+
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Convert-toBackupWindow
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true)]
+        $day,
+        [Parameter(Mandatory=$true)]
+        [int]$hour
+    )
+    
+    
+    if($hour -lt 7){
+        switch ($day)
+        {
+            'Monday' {$backupwindow = 'Sunday to Monday'}
+            'Tuesday'{$backupwindow = 'Monday to Tuesday'}
+            'Wednesday'{$backupwindow = 'Tuesday to Wednesday'}
+            'Thursday' {$backupwindow = 'Wednesday to Thursday'}
+            'Friday' {$backupwindow = 'Thursday to Friday'}
+            'Saturday'{$backupwindow = 'Friday to Saturday'}
+            'Sunday' {$backupwindow = 'Saturday to Sunday'}
+            Default {$backupwindow = 'Not Found'}
+        }
+    }
+    else{
+        switch ($day)
+        {
+            'Monday' {$backupwindow = 'Monday to Tuesday'}
+            'Tuesday'{$backupwindow = 'Tuesday to Wednesday'}
+            'Wednesday'{$backupwindow = 'Wednesday to Thursday'}
+            'Thursday' {$backupwindow = 'Thursday to Friday'}
+            'Friday' {$backupwindow =  'Friday to Saturday'}
+            'Saturday'{$backupwindow = 'Saturday to Sunday'}
+            'Sunday' {$backupwindow ='Sunday to Monday' }
+            Default {$backupwindow = 'Not Found'}
+        }
+    }
+    return $backupwindow
+}
+
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Get-JobData
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Veeam job parameter
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $Job
+    )
+    
+    write-verbose "Getting job data for $($job.name)"    
+    Write-Verbose "`nCollecting when the job is running"
+    $monthlySchedule = $job.BackupTargetOptions.FullBackupMonthlyScheduleOptions
+    $numberinMonth = $monthlySchedule.DayNumberInMonth
+    $fullDayofWeek = $monthlySchedule.DayOfWeek
+
+    #in which hour the job will run
+    $jobTimeHour = $job.ScheduleOptions.StartDateTimeLocal.Hour
+    #in which minute (of the hour) the job will run
+    [string]$jobTimeMinutes = $job.ScheduleOptions.StartDateTimeLocal.Minute
+
+    #if minute only returned one integer add an 0 to the string
+    if($jobTimeMinutes.Length -eq 1){$jobTimeMinutes = '0'+$jobTimeMinutes}
+    $jobHour = "$jobTimeHour`:"+$jobTimeMinutes
+    $backupwindow = Convert-toBackupWindow -day $fullDayofWeek -hour $jobTimeHour
+
+
+    write-verbose "Done collecting when the job is running. Start collecting how big the job is"
+    $vmsinJob = $job.GetObjectsInJob() | sort name
+    #create 64 bit integer, because 2 GB in bytes is too big for 32 bit integer
+    [int64]$jobsize = 0
+    $vmNamesinInjob =""
+    foreach($vm in $vmsinJob){
+        $jobsize+= (Convert-SizeStringtoSizeInteger $vm.ApproxSizeString)
+        $vmNamesinInjob+=$vm.name+';'
+    }
+    #now round to nearest GB
+    [int]$jobsize = $jobsize/1GB
+
+    write-verbose "Done collecting how big the job is. Start collecting on which storage device it is targetting"
+
+    $storagedevice = Get-StorageDevice $job.info.TargetRepositoryId
+
+    
+    
+
+    $jobData =[PSCustomObject]@{
+                Job = $Job.Name
+                'Hour when job is running'=$jobHour
+                'Expected Full Job Size (GB)' = $jobsize
+                'Backup Storage' = $storagedevice
+                'Full: Backup Window' = $backupwindow
+                'Full: Week number'=$numberinMonth
+                'Full: WeekDay' = $fullDayofWeek
+                'VMs in job' = $vmNamesinInjob
+                }
+
+    write-verbose "Found this data for $($job.name) : `n`n $jobData"
+    return $jobData
+}
+
+
+<#
+.Synopsis
+   tries to resolve the storagedevice to a friendly name
+.DESCRIPTION
+   Tries to resolve the storagedevice to a friendly name. This might be nice in case veeam repo's are stored
+   on SMB shares and you are only interested in the server name and not the path.
+#>
+function Get-StorageDevice
+{
+    [CmdletBinding()]
+    [OutputType([string])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $RepoID
+    )
+
+    $repo = Get-VBRBackupRepository | where{$_.id -eq $RepoID}
+    # todo add your own logic here that fits your purpose
+    $storagedevice = ($Repo.name)
+    return $storagedevice
+
+}
+
+#endregion
+
+
+#region 2 module:"initialize variables and add needed modules
+Add-PSSnapin VeeamPSSnapIn
+#endregion
+
+
+#region 3 module: retrieve data
+$backupjobs = Get-VBRJob | Where-Object {$_.IsBackup}
+$rapportData =@()
+foreach($job in $backupjobs){$rapportData+= Get-JobData $job}
+
+$rapportData | select job,'Hour when job is running','Expected Full Job Size (GB)','Backup Storage','Full: Backup Window'`
+                ,'Full: Week number','Full: WeekDay','VMs in job' |
+                Export-Csv '.\Veeambackupschedule.csv' -NoTypeInformation
+
+
+#endregion
